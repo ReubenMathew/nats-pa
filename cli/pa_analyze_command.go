6fce40bb972d4863bbd6ec1aff3ef22a94a404a0
reubenninan <reuben@nats.io>
2024-03-08 11:22:00 -0500

    working

--- cli/pa_analyze_command.go
+++ cli/pa_analyze_command.go
@@ -1,1 +1,19 @@
 package cli
+
+import "github.com/choria-io/fisk"
+
+type PaAnalyzeCmd struct {
+	archivePath string
+}
+
+func configurePaAnalyzeCommand(srv *fisk.CmdClause) {
+	c := &PaAnalyzeCmd{}
+
+	analyze := srv.Command("analyze", "create archive of monitoring data for all servers and accounts").Action(c.analyze)
+	analyze.Arg("archivePath", "path of archive to extract information from and analyze").Required().StringVar(&c.archivePath)
+}
+
+func (c *PaAnalyzeCmd) analyze(_ *fisk.ParseContext) error {
+
+	return nil
+}
